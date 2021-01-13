72b20baf8fbb42ee05cf579f76522c2355a4cbda
Gonçalo Pinto <goncalopinto9@gmail.com>
2021-01-13 12:43:43 +0000

    comentarios e estrelas no server

--- ProjetoFinal/Luis/api-server/controllers/files.js
+++ ProjetoFinal/Luis/api-server/controllers/files.js
@@ -5,17 +5,17 @@ module.exports.list = () => {
     return Files.find().exec()
 }
 
-module.exports.publicFiles = ()=>{
-    return Files.find({privacy:0}).exec()
+module.exports.publicFiles = () => {
+    return Files.find({privacy: 0}).exec()
 }
 
 /* Returns a file record */
 module.exports.lookup = id => {
     return Files.findOne({_id: id}).exec()
-} 
+}
 
 /* Inserts a new paragraph */
-module.exports.insert = (p,path) => {
+module.exports.insert = (p, path) => {
     console.log(JSON.stringify(p))
     var newFile = new Files(p)
     newFile.filepath = path
@@ -27,23 +27,21 @@ module.exports.filesbyUser = id => {
     return Files.find({autor: id}).exec()
 }
 
-module.exports.findByName = name =>{
+module.exports.findByName = name => {
     return Files.findOne({name: name}).exec()
 }
 
 /* Delete a paragraph */
 module.exports.remove = id => {
     return Files.deleteOne({_id: id})
-} 
+}
 
 /* Changes security */
 module.exports.editS = (id) => {
-    Files.findOne({_id: id}).exec()
-    .then((result) => {
-        if(result.privacy == 0) {
+    Files.findOne({_id: id}).exec().then((result) => {
+        if (result.privacy == 0) {
             result.privacy = 1;
-        }
-        else {
+        } else {
             result.privacy = 0;
         }
         return Files.findByIdAndUpdate(id, result, {new: true})
@@ -58,3 +56,65 @@ module.exports.editS = (id) => {
     return Files.findByIdAndUpdate(id, file, {new: true})*/
 }
 
+
+// Adicionar um novo comentario
+module.exports.adicionarComentario = (idR, comentario) => {
+    return Files.update({
+        _id: idR
+    }, {
+        $push: {
+            comentarios: comentario
+        }
+    }).exec()
+}
+
+// Remover um comentario
+module.exports.removerComentario = (idR, idC) => {
+    return Files.update({
+        _id: idR
+    }, {
+        $pull: {
+            "comentarios": {
+                _id: idC
+            }
+        }
+    }).exec()
+}
+
+// Obter estrelas
+module.exports.getEstrelas = (id) => {
+    return Files.find({
+        _id: id
+    }, {
+        _id: 0,
+        estrelas: 1
+    }).exec()
+};
+
+// Adicionar uma nova estrela
+module.exports.incrementarEstrelas = (idR, idU) => {
+    return Files.updateOne({
+        _id: idR
+    }, {
+        $push: {
+            "estrelas.autores": idU
+        },
+        $inc: {
+            "estrelas.numero": 1
+        }
+    }).exec()
+}
+
+// Remover uma estrela
+module.exports.decrementarEstrelas = (idR, idU) => {
+    return Files.updateOne({
+        _id: idR
+    }, {
+        $pull: {
+            "estrelas.autores": idU
+        },
+        $inc: {
+            "estrelas.numero": -1
+        }
+    }).exec()
+}
--- ProjetoFinal/Luis/api-server/models/files.js
+++ ProjetoFinal/Luis/api-server/models/files.js
@@ -1,5 +1,35 @@
 var mongoose = require('mongoose')
 
+var estrelaSchema = new mongoose.Schema({
+    numero: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    autores: [String]
+})
+
+var comentarioSchema = new mongoose.Schema({
+    id: String,
+    data: {
+        type: String,
+        default: new Date().getTime(),
+        required: true
+    },
+    autor: {
+        type: String,
+        required: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    },
+    estrelas: {
+        type: estrelaSchema,
+        required: true
+    }
+});
+
 var FileSchema = new mongoose.Schema({
     date: String,
     autor: String,
@@ -8,7 +38,12 @@ var FileSchema = new mongoose.Schema({
     size: Number,
     privacy: Number,
     filepath: String,
-    descricao: String
+    descricao: String,
+    comentarios: [comentarioSchema],
+    estrelas: {
+        type: estrelaSchema,
+        required: true
+    }
 })
 
 module.exports = mongoose.model('files', FileSchema)
